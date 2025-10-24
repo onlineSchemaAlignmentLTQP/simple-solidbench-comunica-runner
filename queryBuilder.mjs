946a78b32480a92303d027591d9877e40e43fd15
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2025-10-24 14:22:46 +0200

    ignore rule folder

--- queryBuilder.mjs
+++ queryBuilder.mjs
@@ -1,21 +1,24 @@
-import { readdir, readFile, writeFile } from 'node:fs/promises';
+import { readdir, readFile, writeFile } from "node:fs/promises";
 import { join } from "path";
 
 export async function buildQueries(queryFolderPath) {
-    const queryFolder = await readdir(queryFolderPath);
+  const queryFolder = await readdir(queryFolderPath);
 
-    for (const query of queryFolder) {
-        if (query === "parsed") {
-            continue;
-        }
-        const file = join(queryFolderPath, query);
-        const queryStrings = String(await readFile(file)).split("\n\n");
-        let i = 0;
-        const object = {};
-        for (const queryString of queryStrings) {
-            object[`v${i}`] = queryString;
-            i += 1;
-        }
-        await writeFile(join(queryFolderPath, "parsed", `${query}.json`), JSON.stringify(object));
+  for (const query of queryFolder) {
+    if (query === "parsed" || query === "rules") {
+      continue;
     }
+    const file = join(queryFolderPath, query);
+    const queryStrings = String(await readFile(file)).split("\n\n");
+    let i = 0;
+    const object = {};
+    for (const queryString of queryStrings) {
+      object[`v${i}`] = queryString;
+      i += 1;
+    }
+    await writeFile(
+      join(queryFolderPath, "parsed", `${query}.json`),
+      JSON.stringify(object),
+    );
+  }
 }
